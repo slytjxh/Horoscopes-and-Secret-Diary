b54788fa6e71117b630d044511bc3e56c8e2afcc
Irene <airine@Irenes-MacBook-Air.local>
2020-08-13 14:35:01 -0700

    [passport]Add ppConfig

---
+++ config/ppConfig.js
@@ -0,0 +1,35 @@
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const db = require('../models');
+
+/* passport "Serialize" your info make it easier to login
+ -Convert the user based on the id
+ */
+
+passport.serializeUser((user, cb)=>{
+    cb(null, user.id);
+})
+
+// passport "deserializeUser" is going to take the id and look that
+passport.deserializeUser((id, cb) =>{
+    cb(null, id)
+    .catch(cb());
+});
+
+passport.use(new localStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+}, (email, password, cb) =>{
+    db.user.findOne({
+        where: { email}
+    })
+    .then(user => {
+        if(!user || !user.validPassword(password)) {
+            cb(null, false);
+        } else {
+            cb(null, user);
+        }
+    })
+    .catch(cb());
+}
+));
