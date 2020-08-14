57a7c904ec03f805dc831ebb4ee31bd4b49902ca
Irene <airine@Irenes-MacBook-Air.local>
2020-08-14 12:31:38 -0700

    [alert]typo fixed

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  res.render('index', { alerts: req.locals.alerts });
+  res.render('index', { alerts: res.locals.alerts });
 });
 
 
